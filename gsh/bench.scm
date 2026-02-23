03078ca3fe23abb3671a6c9a9c542e4efc9281ce
Jaime Fournier <jaimef@linbsd.org>
2026-02-22 21:50:23 -0700

    we don't want these

--- gsh/bench.scm
+++
@@ -1,7 +0,0 @@
-(declare (block) (standard-bindings) (extended-bindings))
-(begin
-  (begin
-    (load-module "std/sugar")
-    (load-module "std/format")
-    (load-module "std/srfi/1"))
-  (load-module "gsh/bench~0"))
